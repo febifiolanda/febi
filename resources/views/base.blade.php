d3e8ee83ba6295b27387b26c713739f57eb11e64
febifiolanda <febyfiolanda@gamil.com>
2018-09-24 09:03:32 +0700

    Update Halaman Dua

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -40,10 +40,10 @@
                 <a href="/">Home</a>
             </li>
             <li>
-                <a href="#">- Sidebar 1</a>
+                <a href="/halamandua">- Halaman 2</a>
             </li>
             <li>
-                <a href="#">- Sidebar 2</a>
+                <a href="/halamantiga">- Halaman 3</a>
             </li>
         </ul>
     </aside>
